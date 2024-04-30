f2cb764fc2fb7b41e067a676adc8f4e4a273f135
beetrbgus <devbeetrb0214@gmail.com>
2024-04-30 18:00:42 +0900

    feat - 캐시 메모리 추가

---
+++ src/main/java/com/beetrb/redis_study/redis/config/CustomRedisCacheManagerConfig.java
@@ -0,0 +1,41 @@
+package com.beetrb.redis_study.redis.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class CustomRedisCacheManagerConfig {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration =
+            RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(3L))
+            ;
+        return RedisCacheManager.RedisCacheManagerBuilder
+            .fromConnectionFactory(connectionFactory)
+            .cacheDefaults(redisCacheConfiguration)
+            .build();
+    }
+
+    /**
+     * Jedis
+     */
+/*    @Bean
+    public RedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory(
+            new RedisStandaloneConfiguration("server",6379)
+        );
+    }*/
+}
