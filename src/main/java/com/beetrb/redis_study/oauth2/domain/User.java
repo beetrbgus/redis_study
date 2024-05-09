69ae83cd8f8e79ae5bdef36cb0d7017b0b5d0a1d
beetrbgus <devbeetrb0214@gmail.com>
2024-05-09 17:50:56 +0900

    feat - User 프로퍼티 중 LocalDateTime에 Json직/역직렬화 어노테이션 추가

--- src/main/java/com/beetrb/redis_study/oauth2/domain/User.java
+++ src/main/java/com/beetrb/redis_study/oauth2/domain/User.java
@@ -2,13 +2,17 @@ package com.beetrb.redis_study.oauth2.domain;
 
 import com.beetrb.redis_study.oauth2.domain.provider.ProviderUserInfo;
 import com.beetrb.redis_study.oauth2.domain.provider.SocialType;
-import com.github.f4b6a3.tsid.Tsid;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.github.f4b6a3.tsid.TsidCreator;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -19,6 +23,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 
+@ToString
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -36,10 +41,16 @@ public class User {
     private UserRole role;
 
     @CreatedDate
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdAt;
     @LastModifiedDate
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime updatedAt;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime lastLoginedTime;
 
     public static User create(ProviderUserInfo providerUser) {
